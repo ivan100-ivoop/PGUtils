51575dbf6361d62728c8b5f9923e2fe9a21054a3
ivan100-ivoop <83706102+ivan100-ivoop@users.noreply.github.com>
2024-01-05 20:44:28 +0200

    Update

--- src/main/java/com/github/pgutils/PGUtils.java
+++ src/main/java/com/github/pgutils/PGUtils.java
@@ -2,8 +2,9 @@ package com.github.pgutils;
 
 import com.github.pgutils.commands.PGCommand;
 import com.github.pgutils.commands.PGTabComplete;
+import com.github.pgutils.hooks.PGLobbyHook;
+
 import org.bukkit.Bukkit;
-import org.bukkit.command.CommandExecutor;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,24 +12,27 @@ import java.io.File;
 import java.util.logging.Logger;
 
 public final class PGUtils extends JavaPlugin {
-
-    public Logger logger = Bukkit.getLogger();
-    public String prefix;
-
-    @Override
-    public void onEnable() {
-        saveDefaultConfig();
-
-        prefix = getConfig().getString("prefix", "&7[&e&lPGUtils&7] ");
-
-        if(!new File(getDataFolder(), "database").exists())
-            new File(getDataFolder(), "database").mkdir();
-
-
-        getCommand("pgutils").setExecutor(new PGCommand());
-        getCommand("pgutils").setTabCompleter(new PGTabComplete());
-
-    }
+	public Logger logger = Bukkit.getLogger();
+	public String prefix;
+	public static File database = null;
+	
+	@Override
+	public void onEnable() {
+		saveDefaultConfig();
+		
+		prefix = getConfig().getString("prefix", "&7[&e&lPGUtils&7] ");
+		
+		if(!new File(getDataFolder(), "database").exists())
+			new File(getDataFolder(), "database").mkdir();
+		
+		database = new File(getDataFolder(), "database");
+		
+		getCommand("pgutils").setExecutor(new PGCommand());
+		getCommand("pgutils").setTabCompleter(new PGTabComplete());
+		
+		Bukkit.getPluginManager().registerEvents(new PGLobbyHook(), this);
+		
+	}
 
     @Override
     public void onDisable() {}
